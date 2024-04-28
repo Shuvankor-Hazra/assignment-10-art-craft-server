1b9eaf309fbd31e7449ae285d29967a9c3c65442
Shuvankr Hazra <shuvo.s.ovuhs@gmail.com>
2024-04-29 03:23:52 +0600

    Craft Details added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -31,6 +31,13 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+
+    app.get("/crafts/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const craft = await craftsCollection.findOne(query);
+        res.send(craft);
+      });
 
     app.post("/crafts", async (req, res) => {
       const newCraft = req.body;
