23aee1a976ec285f69a6065fe9ae3d69cc483a8c
Shuvankr Hazra <shuvo.s.ovuhs@gmail.com>
2024-04-30 18:42:43 +0600

    subcategory & email added

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fi65pdm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+console.log(uri);
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -22,9 +24,10 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const craftsCollection = client.db("craftsDB").collection("crafts");
+    const subCraftsCollection = client.db("craftsDB").collection("subCrafts");
 
     app.get("/crafts", async (req, res) => {
       const cursor = craftsCollection.find();
@@ -32,12 +35,32 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/subCrafts", async (req, res) => {
+      const cursor = subCraftsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/myCrafts/:subcategoryName", async (req, res) => {
+      const id = req.params.subcategoryName;
+      const query = { subcategoryName:id };
+      const result = await craftsCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.get("/myArtCraft/:email", async (req, res) => {
+      const id = req.params.email;
+      const query = { userEmail:id };
+      const result = await craftsCollection.find(query).toArray();
+      res.send(result);
+    });
+
     app.get("/crafts/:id", async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: new ObjectId(id) };
-        const craft = await craftsCollection.findOne(query);
-        res.send(craft);
-      });
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const craft = await craftsCollection.findOne(query);
+      res.send(craft);
+    });
 
     app.post("/crafts", async (req, res) => {
       const newCraft = req.body;
@@ -45,6 +68,9 @@ async function run() {
       const result = await craftsCollection.insertOne(newCraft);
       res.send(result);
     });
+
+    //Users
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
